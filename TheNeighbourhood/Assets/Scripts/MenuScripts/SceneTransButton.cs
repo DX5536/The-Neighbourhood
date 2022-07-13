ecd49950dbdb917352490351330275e9e3b367cc
Hai Lam Dang <hai.dang@ue-germany.de>
2022-07-13 21:09:05 +0200

    [FIX] ButtonSFX got cuttoff

--- TheNeighbourhood/Assets/Scripts/MenuScripts/SceneTransButton.cs
+++ TheNeighbourhood/Assets/Scripts/MenuScripts/SceneTransButton.cs
@@ -1,9 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransButton : MonoBehaviour
+public class SceneTransButton: MonoBehaviour
 {
     [SerializeField]
     private string sceneName_READ_ONLY;
@@ -11,24 +10,30 @@ public class SceneTransButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ChangeSceneButton(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
-
+        StartCoroutine(SmallDelayForSFX(sceneIndex));
     }
 
     public void ExitGameButton()
     {
         Debug.Log("Exit successfully");
         Application.Quit();
+    }
+
+    IEnumerator SmallDelayForSFX(int sceneIndex)
+    {
+        yield return new WaitForSeconds(0.2f);
+        //Debug.Log("Finish Delay");
+        SceneManager.LoadScene(sceneIndex);
     }
 }
